58c710bffa82e6261e9bf3e5be1010148b785707
Leon <yanglyu.leon.7@gmail.com>
2020-09-13 17:08:27 -0700

    reviews

---
+++ leetcode/lvl3/lc0797/solution-0-bfs/solution.go
@@ -0,0 +1,32 @@
+package lc0797
+
+// https://leetcode.com/problems/all-paths-from-source-to-target/
+// Time Complexity:		O()
+// Space Complexity:	O()
+// Reference
+// 	https://leetcode.com/problems/all-paths-from-source-to-target/discuss/752384/Golang-Simple-BFS
+func allPathsSourceTarget(graph [][]int) [][]int {
+	paths := [][]int{}
+	ans := [][]int{}
+	paths = append(paths, []int{0})
+	for len(paths) > 0 {
+		path := paths[0]
+		paths = paths[1:]
+		// last element in the `path`
+		cur := path[len(path)-1]
+		// to check if the last element is the end
+		if cur == len(graph)-1 {
+			ans = append(ans, path)
+			continue
+		}
+
+		for _, next := range graph[cur] {
+			copied := make([]int, len(path)+1)
+			copy(copied, path)
+			copied[len(path)] = next
+			paths = append(paths, copied)
+		}
+	}
+
+	return ans
+}
---
+++ leetcode/lvl3/lc0797/solution-0-dfs-recursive/solution.go
@@ -0,0 +1,34 @@
+package lc0797
+
+// https://leetcode.com/problems/all-paths-from-source-to-target/
+// Time Complexity:		O()
+// Space Complexity:	O()
+// Reference
+// 	https://leetcode.com/problems/all-paths-from-source-to-target/discuss/615535/Golang-DFS
+func allPathsSourceTarget(graph [][]int) [][]int {
+	paths := [][]int{}
+
+	dfs(0, []int{0}, len(graph)-1, graph, &paths)
+
+	return paths
+}
+
+func dfs(cur int,
+	path []int,
+	destination int,
+	graph [][]int,
+	paths *[][]int) {
+
+	if cur == destination {
+		copied := make([]int, len(path))
+		copy(copied, path)
+		*paths = append(*paths, copied)
+		return
+	}
+
+	for _, next := range graph[cur] {
+		path = append(path, next)
+		dfs(next, path, destination, graph, paths)
+		path = path[:len(path)-1]
+	}
+}

0dce512c13140292dc901fde04e670f89df91a7e
Leon <yanglyu.leon.7@gmail.com>
2021-04-15 23:27:02 -0700

    review

---
+++ src/leetcode/lvl1/lc0509/soln0-dp-0d/soln.go
@@ -0,0 +1,27 @@
+// @author: Leon
+// https://leetcode.com/problems/fibonacci-number/
+//
+// Time Complexity:     O(`n`)
+// Space Complexity:    O(1)
+func fib(n int) int {
+    if n == 0{
+        return 0
+    }
+    
+    if n == 1{
+        return 1
+    }
+    
+    prev2 := 0
+    prev1 := 1
+    
+    sum := 0
+    
+    for i := 2; i <= n; i++ {
+        sum = prev2 + prev1
+        prev2 = prev1
+        prev1 = sum
+    }
+    
+    return sum
+}
